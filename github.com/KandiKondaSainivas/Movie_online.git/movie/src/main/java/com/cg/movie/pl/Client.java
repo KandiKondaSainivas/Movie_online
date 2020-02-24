7507dcad562e8c10f3959b4897c6ddb8fd4c4891
ADMIN <ADMIN@EEE-NEWLAB-215>
2020-02-24 11:34:54 +0530

    Updated

--- github.com/KandiKondaSainivas/Movie_online.git/movie/src/main/java/com/cg/movie/pl/Client.java
+++ github.com/KandiKondaSainivas/Movie_online.git/movie/src/main/java/com/cg/movie/pl/Client.java
@@ -12,59 +12,79 @@ public class Client {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		AdminService adminService = new AdminServiceImpl();
-		Theater theater = null;
+		Theater theater = new Theater();
 		int choice = 0;
 		while (choice != 3) {
-			System.out.println("1.Add Theater\n2.Delete Theater");
-			System.out.println("Enter your choice");
-			choice = scanner.nextInt();
+			System.out.println("1.Add Theater\n2.Delete Theater\n3.Exit");
+			try {
+				System.out.println("Enter your choice");
 
+				choice = scanner.nextInt();
+			} catch (Exception e) {
+				System.err.println("Enter Correct Input");
+				scanner.nextLine();
+			}
 			switch (choice) {
 
 			case 1:
-				System.out.println("Enter theater Id");
-				int theaterId = scanner.nextInt();
-				System.out.println("Enter theater Name");
-				scanner.nextLine();
-				String theaterName = scanner.nextLine();
-				System.out.println("Enter theater City");
-				String theaterCity = scanner.nextLine();
-				System.out.println("Enter Manager Name");
-				String managerName = scanner.nextLine();
-				System.out.println("Enter Manager Contact");
-				String managerContact = scanner.nextLine();
-
-				theater = new Theater();
-
-				theater.setTheaterId(theaterId);
-				theater.setTheaterName(theaterName);
-				theater.setTheaterCity(theaterCity);
-				theater.setManagerName(managerName);
-				theater.setManagerContact(managerContact);
 
 				try {
-					Theater t = adminService.addTheater(theater);
+					System.out.println("Enter theater Id");
+					int theaterId = scanner.nextInt();
+					System.out.println("Enter theater Name");
+					scanner.nextLine();
+					String theaterName = scanner.nextLine();
+					System.out.println("Enter theater City");
+					String theaterCity = scanner.nextLine();
+					System.out.println("Enter Manager Name");
+					String managerName = scanner.nextLine();
+					System.out.println("Enter Manager Contact");
+					String managerContact = scanner.nextLine();
+
+					theater.setTheaterId(theaterId);
+					theater.setTheaterName(theaterName);
+					theater.setTheaterCity(theaterCity);
+					theater.setManagerName(managerName);
+					theater.setManagerContact(managerContact);
+
+					adminService.addTheater(theater);
 					System.out.println("Added theater successfully ");
 				} catch (MovieException e) {
 					System.err.println(e.getMessage());
+				} catch (Exception e) {
+					System.err.println("Enter Correct Input");
+					scanner.nextLine();
 				}
 
 				break;
 
 			case 2:
-				System.out.println("Enter theater id to delete");
-				int theaterId1 = scanner.nextInt();
-				try {
-					boolean flag = adminService.deleteTheater(theaterId1);
-					if (flag == true) {
-						System.out.println(" Theater Deleted successfully");
+
+				if (theater.getTheaterId() > 0) {
+					try {
+
+						System.out.println("Enter theater id to delete");
+						int theaterId1 = scanner.nextInt();
+
+						boolean flag = adminService.deleteTheater(theaterId1);
+						if (flag == true) {
+							System.out.println(" Theater Deleted successfully");
+						}
+
+					} catch (MovieException e) {
+						System.err.println(e.getMessage());
+					} catch (Exception e) {
+						System.err.println("Enter Correct Input");
+						scanner.nextLine();
 					}
-
-				} catch (MovieException e) {
-					System.err.println(e.getMessage());
+				} else {
+					System.err.println("First add theater then we can delete the theater");
 				}
-				break;
 
+				break;
+			case 3:
+				System.out.println("Thank You");
+				return;
 			}
 
 		}
